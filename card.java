74cf3d9374cc940e5729c74d9ad3a673d67f6b61
StillLemon <aidengrossman123@gmail.com>
2024-10-23 20:21:37 -0600

    card.java

---
+++ card.java
@@ -0,0 +1,12 @@
+public class card {
+    String suit;
+    int value;
+    String name;
+    boolean ace;
+    public card(String suit, int value, String name, boolean ace){
+        this.suit = suit;
+        this.value = value;
+        this.name = name;
+        this.ace = ace;
+    }
+}
